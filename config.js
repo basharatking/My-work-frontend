31349eef0cf9d6f2c6aa3b50cf5fc91456d8bb1f
basharatking <86537190+basharatking@users.noreply.github.com>
2026-05-15 02:58:51 +0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.CATCHPDF_CONFIG = {
-  API_BASE: "https://aapka-backend-url.replit.co", // Yahan apna URL lagayein
-  BRAND_NAME: "CatchPDF",
-  FREE_LIMIT_MB: 25,
-  GA_ID: "G-XXXXXXXXXX" 
-};
