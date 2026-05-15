273c47776e372b539ca78ce788d31504367d2bb3
basharatking <86537190+basharatking@users.noreply.github.com>
2026-05-15 20:08:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CATCHPDF_CONFIG = {
-  API_BASE:      "",
+  API_BASE:      "https://my-work-backend--mrjb54890.replit.app",
   FREE_LIMIT_MB: 25,
   GA_ID:         "G-XXXXXXXXXX",
   BRAND:         "CatchPDF",
