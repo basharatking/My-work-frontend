7059d717ba2cbc73016ef8e563d577144dc3e1da
basharatking <86537190+basharatking@users.noreply.github.com>
2026-05-22 01:43:52 +0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.CATCHPDF_CONFIG = {
-  API_BASE:      "https://my-work-backend--mrjb54890.replit.app",
-  FREE_LIMIT_MB: 25,
-  GA_ID:         "G-XXXXXXXXXX",
-  BRAND:         "CatchPDF",
-  VERSION:       "1.0.0",
-};
