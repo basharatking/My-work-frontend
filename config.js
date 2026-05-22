710a04651726fc66e155d289cb313581c851e88f
basharatking <86537190+basharatking@users.noreply.github.com>
2026-05-23 00:55:54 +0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.PDFPILOT_CONFIG = {
-  API_BASE:      "https://pdfpilot-backend.replit.app",
-  FREE_LIMIT_MB: 25,
-  GA_ID:         "G-XXXXXXXXXX",
-  BRAND:         "PDFPilot",
-  VERSION:       "2.0.0",
-};
