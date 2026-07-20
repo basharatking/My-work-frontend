bd39fc32bad2a718dad1859c8487d3eb60b85d71
basharatking <86537190+basharatking@users.noreply.github.com>
2026-07-20 15:50:20 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -5,4 +5,8 @@ window.RUNDOCS_CONFIG = {
   VERSION: "1.0.0",
   SUPABASE_URL: "https://qmiksanrefiplaicmnii.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtaWtzYW5yZWZpcGxhaWNtbmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1Mjk4MzMsImV4cCI6MjA5OTEwNTgzM30.GKnp1noW1JK1lcadK9BfB_tgQ06eGRpdd02N0hX193o",
+  PADDLE_CLIENT_TOKEN: "test_04fc057b49983076074df019005",
+  PADDLE_PRO_MONTHLY: "pri_01kxz75z3t7jfw75dkmm0fxk4p",
+  PADDLE_PRO_YEARLY: "pri_01kxz7j9tn793zv33rnhcwfnby",
+  PADDLE_SANDBOX: true,
 };
